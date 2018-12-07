63d756d3c91a1413674f9cf155f8cb892d97ef17
Ubuntu <ubuntu@ip-172-31-20-86.us-east-2.compute.internal>
2018-12-07 15:04:25 +0000

    change IP for linux

--- bin/changeip.sh
+++ bin/changeip.sh
@@ -2,12 +2,21 @@
 
 ### IP Change 1=old_IP 2=new_IP
 if [ \("$1" != "" -a "$2" != ""\) ]; then
-	sed -i '.bak' "s/$1/$2/g" ../bootstrap.sh
-	sed -i '.bak' "s/$1/$2/g" ../Vagrantfile
-	sed -i '.bak' "s/$1/$2/g" ../000-default.conf
-	sed -i '.bak' "s/$1/$2/g" *.sh	
-	rm *.bak
-	rm ../*.bak
+	
+	# Linux and FreeBSD have different syntax for sed
+	if [[ "$OSTYPE" == "linux-gnu" ]]; then
+		sed -i -e "s/$1/$2/g" ../bootstrap.sh
+		sed -i -e "s/$1/$2/g" ../Vagrantfile
+		sed -i -e "s/$1/$2/g" ../000-default.conf
+		sed -i -e "s/$1/$2/g" *.sh
+	else
+		sed -i '.bak' "s/$1/$2/g" ../bootstrap.sh
+		sed -i '.bak' "s/$1/$2/g" ../Vagrantfile
+		sed -i '.bak' "s/$1/$2/g" ../000-default.conf
+		sed -i '.bak' "s/$1/$2/g" *.sh	
+	fi
+	rm -f *.bak
+	rm -f ../*.bak
 else
 	echo "USAGE: $0 <old_ip> <new_ip>"
 fi
